c873c2dbf1af2d0828d07b070e0d539101685cfe
NEYAZUDDIN5050 <132392596+NEYAZUDDIN5050@users.noreply.github.com>
2024-08-20 21:26:01 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,36 @@
+
+
+
+const reviews = [
+  {
+    id: 1,
+    name: "Neyaz Uddin",
+    job: "Community Creater",
+    image: "https://media.licdn.com/dms/image/v2/D5603AQHuu5eHmcaSXg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1712038493400?e=1729728000&v=beta&t=tI32Ose0Tb0CrEDxIxJF5XzheRpsBotWukWrnBQYAb4",
+    text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
+  },
+  {
+    id: 2,
+    name: "Francis Maina",
+    job: "React Developers",
+    image: "https://media.licdn.com/dms/image/v2/D4E35AQEh4qG9VEhx7A/profile-framedphoto-shrink_400_400/profile-framedphoto-shrink_400_400/0/1718184846801?e=1724770800&v=beta&t=9DnGIdSm1arWYkcA7aOkd_lu_gt8uztDC86PoGId9K8",
+    text: "Heyy, I am from KENEYA. I have been working with this community for several months now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
+  },
+  {
+    id: 3,
+    name: "Mohit",
+    job: "Backend Developers",
+    image: "https://i.pinimg.com/474x/49/5d/96/495d96c8f7af75e656d77458f88bed9f.jpg",
+    text: "Heyy, I was connected in this community for past few month.I get a some good resources of my essential Projects, I really enjoyed the company.",
+  },
+  {
+    id: 4,
+    name: "Ahtesham Firoz",
+    job: "Computer Science student",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8FEVu28E50m6zXvLfR66Pdtxb4FvvqmaoduH0p1woHh28qEZ6dcGEemrAGItct2wByZA&usqp=CAU",
+    text: "I am glade to be here .I found a really Good stuffs here. and I also Learn a react js on a daily basis.",
+  },
+ 
+];
+
+export default reviews;
